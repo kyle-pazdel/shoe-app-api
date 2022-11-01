9b7c7b11c25cbbaffe1779d5cc3f47b50f465e99
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-01 15:49:52 -0700

    Adds seed data for shoes in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ while i <= 12
   i += 1
 end
 
-Shoe.create(name: "Pointed Toe Shoe", color: "red", style: "stilleto", description: "A fun and fancy 5 1/2 inch heel for some fun and class")
+Shoe.create(name: "Pointed Toe Shoe", color: "red", style: "stilleto", description: "A fun and fancy 5 1/2 inch heel for some fun and class", image: "https://www.thecelebritydresses.com/media/catalog/product/cache/1/image/363x/040ec09b1e35df139433887a97daa66f/w/o/women_s-red-sexy-pointed-toe-party-shoes-with-stiletto-heels.jpg")
 
-Shoe.create(name)
+Shoe.create(name: "Beach Heel", color: "beige", style: "wedge", description: "A fun and flirty casual heel, perfect for any occasion.", image: "https://images.dsw.com/is/image/DSWShoes/525807_252_ss_01?impolicy=qlt-medium-high&imwidth=640&imdensity=2")
+
+Shoe.create(name: "Chunky-Heeled Boot", color: "Brown", style: "boot", description: "Look good and get the job done!", image: "https://images.dsw.com/is/image/DSWShoes/510037_231_ss_01?impolicy=qlt-medium-high&imwidth=640&imdensity=2")
