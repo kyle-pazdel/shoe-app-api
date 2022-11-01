c37e8cb5b7bc23801e117dcd1c7bab5ffafda8a1
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-01 15:40:36 -0700

    Adds size count seed data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+i = 5
+while i <= 12
+  Size.create(number: i)
+  i += 1
+end
